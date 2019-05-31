368fc49223c3439b085bf39a12365ecc3a4ae7b3
Robert Butler <robwbutler@gmail.com>
2019-05-31 13:35:04 -0400

    branch using JCanvas to build the duck

--- public/js/index.js
+++ public/js/index.js
@@ -97,3 +97,126 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+// This demo will work on iOS, Android, and desktop browsers
+// Draw the outline of an unclosed path (ie a duck part), click to apply a random color from the array
+var headColor = "yellow";
+var bodyColor = "yellow";
+var billColor = "orange";
+
+$("canvas")
+  .drawEllipse({
+    layer: true,
+    name: "body",
+    strokeStyle: "#000",
+    strokeWidth: 3,
+    fillStyle: "yellow",
+    x: 160,
+    y: 160,
+    width: 200,
+    height: 150,
+    click: function(layer) {
+      $(this).animateLayer(
+        layer,
+        {
+          fillStyle: $("#color").val()
+        },
+        0
+      );
+      bodyColor = $("#color").val();
+    }
+  })
+  .drawEllipse({
+    layer: true,
+    name: "head",
+    strokeStyle: "#000",
+    strokeWidth: 3,
+    fillStyle: "yellow",
+    x: 110,
+    y: 80,
+    width: 120,
+    height: 120,
+    click: function(layer) {
+      $(this).animateLayer(
+        layer,
+        {
+          fillStyle: $("#color").val()
+        },
+        0
+      );
+      headColor = $("#color").val();
+    }
+  })
+  .drawEllipse({
+    layer: true,
+    name: "lefteye",
+    strokeStyle: "black",
+    strokeWidth: 5,
+    fillStyle: "white",
+    x: 70,
+    y: 70,
+    width: 15,
+    height: 15
+  })
+  .drawEllipse({
+    layer: true,
+    name: "righteye",
+    strokeStyle: "black",
+    strokeWidth: 5,
+    fillStyle: "white",
+    x: 120,
+    y: 70,
+    width: 15,
+    height: 15
+  })
+  .drawPath({
+    layer: true,
+    name: "bill",
+    strokeStyle: "#000",
+    strokeWidth: 3,
+    fillStyle: "orange",
+    click: function(layer) {
+      $(this).animateLayer(
+        layer,
+        {
+          fillStyle: $("#color").val()
+        },
+        0
+      );
+      billColor = $("#color").val();
+    },
+    p1: {
+      type: "bezier",
+      x1: 65,
+      y1: 90, // Start point
+      cx1: 105,
+      cy1: 30, // Control point
+      cx2: 90,
+      cy2: 90, // Control point
+      x2: 125,
+      y2: 90 // Start/end point
+    },
+    p2: {
+      type: "bezier",
+      x1: 65,
+      y1: 90,
+      cx1: 0,
+      cy1: 120,
+      cx2: 45,
+      cy2: 150,
+      x2: 125,
+      y2: 90
+    }
+  });
+
+$("#head_test").on("click", function() {
+  alert("Quack! My head is " + headColor);
+});
+
+$("#body_test").on("click", function() {
+  alert("Quack! My body is " + bodyColor);
+});
+
+$("#bill_test").on("click", function() {
+  alert("Quack! My bill is " + billColor);
+});
