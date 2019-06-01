09771555f2e477f491be18ded4b865ef2570ff11
Robert Butler <robwbutler@gmail.com>
2019-06-01 11:51:11 -0400

    proof of concept for squeezed duck

--- public/js/index.js
+++ public/js/index.js
@@ -105,7 +105,7 @@ var bodyColor = "yellow";
 var billColor = "orange";
 var hatOn = false;
 
-$("canvas")
+$("#canvas1")
   .drawEllipse({
     layer: true,
     name: "body",
@@ -236,6 +236,7 @@ $("#addhat").on("click", function() {
   } else {
     $("canvas").drawImage({
       name: "hat",
+      groups: ["duck"],
       layer: true,
       source: "./hats/l3helmet.svg",
       x: 155,
@@ -243,4 +244,99 @@ $("#addhat").on("click", function() {
     });
     hatOn = true;
   }
+});
+
+$("#canvas2")
+  .drawEllipse({
+    layer: true,
+    groups: ["duck"],
+    name: "body",
+    strokeStyle: "#000",
+    strokeWidth: 3,
+    fillStyle: "yellow",
+    x: 210,
+    y: 230,
+    width: 200,
+    height: 150
+  })
+  .drawEllipse({
+    layer: true,
+    groups: ["duck"],
+    name: "head",
+    strokeStyle: "#000",
+    strokeWidth: 3,
+    fillStyle: "yellow",
+    x: 160,
+    y: 150,
+    width: 120,
+    height: 120
+  })
+  .drawEllipse({
+    layer: true,
+    groups: ["duck"],
+    name: "lefteye",
+    strokeStyle: "black",
+    strokeWidth: 5,
+    fillStyle: "white",
+    x: 120,
+    y: 140,
+    width: 14,
+    height: 18
+  })
+  .drawEllipse({
+    layer: true,
+    groups: ["duck"],
+    name: "righteye",
+    strokeStyle: "black",
+    strokeWidth: 5,
+    fillStyle: "white",
+    x: 170,
+    y: 140,
+    width: 14,
+    height: 18
+  })
+  .drawPath({
+    layer: true,
+    name: "bill",
+    strokeStyle: "#000",
+    strokeWidth: 3,
+    fillStyle: "orange",
+    p1: {
+      type: "bezier",
+      x1: 115,
+      y1: 160, // Start point
+      cx1: 155,
+      cy1: 100, // Control point
+      cx2: 140,
+      cy2: 160, // Control point
+      x2: 175,
+      y2: 160 // Start/end point
+    },
+    p2: {
+      type: "bezier",
+      x1: 115,
+      y1: 160,
+      cx1: 50,
+      cy1: 190,
+      cx2: 95,
+      cy2: 220,
+      x2: 175,
+      y2: 160
+    }
+  });
+
+$("#squeeze").on("click", function() {
+  $("#canvas2")
+    .animateLayerGroup("duck", {
+      scaleY: 0.8
+    })
+    .animateLayer("bill", {
+      scaleY: 0.95
+    })
+    .animateLayerGroup("duck", {
+      scaleY: 1
+    })
+    .animateLayer("bill", {
+      scaleY: 1
+    });
 });
